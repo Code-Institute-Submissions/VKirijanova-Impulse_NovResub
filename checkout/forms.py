70061d1929f0c184b5ba946d28d7527e9828b46b
VKirijanova <v.kirijanova@gmail.com>
2021-09-27 22:08:24 +0000

    added checkout templates and views

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Order
+from .models import Purchase
 
 
 class PurchaseForm(forms.ModelForm):
--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import PurchaseForm
+
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "You didn't choose any packages yet.")
+        return redirect(reverse('packages'))
+
+    purchase_form = PurchaseForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'purchase_form': purchase_form,
+    }
+
+    return render(request, template, context)

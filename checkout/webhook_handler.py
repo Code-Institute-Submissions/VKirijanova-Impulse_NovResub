52d81578bf8d797a62ae6fb552987d8de2b28590
VKirijanova <v.kirijanova@gmail.com>
2021-09-28 23:04:17 +0000

    updated webhook handler to handle account

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from .models import Purchase, PurchaseLineItem
 from packages.models import Package
+from accounts.models import UserAccount
 
 import json
 import time
@@ -38,6 +39,20 @@ class StripeWH_Handler:
             if value == "":
                 shipping_details.address[field] = None
 
+        account = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            account = UserAccount.objects.get(user__username=username)
+            if save_info:
+                account.default_phone_number = shipping_details.phone
+                account.default_country = shipping_details.address.country
+                account.default_postcode = shipping_details.address.postal_code
+                account.default_town_or_city = shipping_details.address.city
+                account.default_street_address1 = shipping_details.address.line1
+                account.default_street_address2 = shipping_details.address.line2
+                account.default_county = shipping_details.address.state
+                account.save()
+
         purchase_exists = False
         attempt = 1
         while attempt <= 5:
@@ -70,6 +85,7 @@ class StripeWH_Handler:
             try:
                 purchase = Purchase.objects.create(
                     full_name=shipping_details.name,
+                    user_account=account,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,

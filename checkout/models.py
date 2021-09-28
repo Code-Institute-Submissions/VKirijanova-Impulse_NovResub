f240d0b6784c5a9ace4a68297e08ea9e82675ffb
VKirijanova <v.kirijanova@gmail.com>
2021-09-28 07:49:40 +0000

    added country model to check out

--- checkout/models.py
+++ checkout/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 
+from django_countries.fields import CountryField
+
 from packages.models import Package
 
 class Purchase(models.Model):
@@ -16,7 +18,7 @@ class Purchase(models.Model):
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     original_bag = models.TextField(null=False, blank=False, default='')

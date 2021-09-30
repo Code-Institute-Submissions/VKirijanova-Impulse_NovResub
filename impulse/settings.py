26814163789aa54224f8ec47f4921a49137c0ae7
VKirijanova <v.kirijanova@gmail.com>
2021-09-30 04:01:35 +0000

    removed secret key and set up debug

--- impulse/settings.py
+++ impulse/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5k707^99vx57j_0)8_i_!(%rk*t4%uwvaf4r-alee3@o))kg8a'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['vkirijanova-impulse.herokuapp.com', 'localhost']
 

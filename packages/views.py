586849201cfc2a0b0cc22c5b82947db85d2e98d1
VKirijanova <v.kirijanova@gmail.com>
2021-11-03 15:35:36 +0000

    add delete function for admin users

--- packages/views.py
+++ packages/views.py
@@ -34,9 +34,9 @@ def add_package(request):
     if request.method == 'POST':
         form = PackageForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            package = form.save()
             messages.success(request, 'Successfully added new package.')
-            return redirect(reverse('add_package'))
+            return redirect(reverse('package_detail', args=[package.id]))
         else:
             messages.error(request, 'Failed to add package. Please ensure the form is valid.')
     else:
@@ -70,3 +70,13 @@ def edit_package(request, package_id):
     }
 
     return render(request, template, context)
+
+
+def delete_package(request, package_id):
+    package = get_object_or_404(Package, pk=package_id)
+    package.delete()
+    messages.success(request, 'Successfully deleted package')
+    return redirect(reverse('packages'))
+
+
+
